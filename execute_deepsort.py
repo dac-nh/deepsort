19eb18e1efa72ad54233d049c1847a8223e15e23
= <=>
2021-07-05 16:24:10 +0700

    update

--- execute_deepsort.py
+++ execute_deepsort.py
@@ -4,18 +4,6 @@
 from deep_sort import nn_matching
 from deep_sort.detection import Detection
 from deep_sort.tracker import Tracker
-from tools import generate_detections as gdet
-
-# Definition of the parameters
-MAX_COSINE_DISTANCE = 0.4
-NN_BUDGET = None
-MODEL_FILENAME = 'model_data/mars-small128.pb'
-NMS_MAX_OVERLAP = 1.0
-
-# initialize tracker
-encoder = gdet.create_box_encoder(MODEL_FILENAME, batch_size=1)
-metric = nn_matching.NearestNeighborDistanceMetric("cosine", MAX_COSINE_DISTANCE, NN_BUDGET)
-tracker = Tracker(metric)
 
 
 def execute_tracker(frame, bboxes, scores, class_names):
@@ -29,6 +17,7 @@ def execute_tracker(frame, bboxes, scores, class_names):
     Parameters:
     -----------
     frame, bboxes, scores, class_names
+    bboxes: x_min, y_min, width, height
     Returns:
     -----------
     items_arr
@@ -37,16 +26,16 @@ def execute_tracker(frame, bboxes, scores, class_names):
     features = encoder(frame, bboxes)
     detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in
                   zip(bboxes, scores, class_names, features)]
+
     # boxs = np.array([d.tlwh for d in detections]) # Get the box locations
     # scores = np.array([d.confidence for d in detections])
     # classes = np.array([d.class_name for d in detections])
-    # indices = preprocessing.non_max_suppression(boxs, classes, NMS_MAX_OVERLAP, scores) # have use non-max-suppression before
+    # indices = preprocessing.non_max_suppression(boxs, classes, 0.3, 0) # have use non-max-suppression before
     # detections = [detections[i] for i in indices]
 
     # Call the tracker
     tracker.predict()
     tracker.update(detections)
-
     # Get all objects with track id
     items_arr = {}
     for track in tracker.tracks:
